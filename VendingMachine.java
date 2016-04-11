f506f76482ab39834d6550de4f94736ff761ebb8
cagdasoztekin <cagdas.oztekin@ug.bilkent.edu.tr>
2016-04-11 13:08:19 +0300

    flawless code with comments

--- VendingMachine.java
+++ VendingMachine.java
@@ -6,25 +6,39 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.InputMismatchException;
 
-public class VendingMachine{
+public class VendingMachine implements Runnable{
   //Local varialbles that will be used by either the client and/or the server
   //Will be used by client instantiations only
   Socket clientSocket = null;
   HashMap<Integer, Integer> receivedItems = null;
   //Will be used by server instantiations only
   ServerSocket serverSocket = null;
-  HashMap<Integer, String> itemNames = null;
-  HashMap<Integer, Integer> itemStock = null;
+  //Static HashMap's unlike the first assignment so that all threads can share these two
+  static HashMap<Integer, String> itemNames = null;
+  static HashMap<Integer, Integer> itemStock = null;
   //Will be used by both client and server instantiations
   Socket connection = null;
   ObjectOutputStream out = null;
   ObjectInputStream in = null;
   String message = null;
 
+  //Number of currently active threads
+  static int numOfThreads = 0;
+  //Number of maximum threads allowed
+  final int MAX_NUM_OF_THREADS = 3;
+
+  //A constructor to VendingMachine that takes the established connection's socket as parameter to reuse it
+  VendingMachine(Socket connection){
+    //Pass the current socket to the thread
+    this.connection = connection;
+    //Increment the count of active threads
+    numOfThreads += 1;
+  }
+
   //Empty constuctor
   VendingMachine(){};
 
-  public static void main(String args[]){
+  public static void main(String args[]) throws Exception{
     //This will be a client
     if (args.length == 2){
       VendingMachine client = new VendingMachine();
@@ -36,8 +50,9 @@ public class VendingMachine{
       //Read the inventory from the text file
       server.readItemList();
       //Keep on running
-      while (true)
-        server.runServer(args);
+      server.runServer(args);
+      //Print the following message, if the server reaches here it means there is another server running on the same port
+      System.out.println("There is already a server instance running on this port [" + args[0] + "].\nTerminating...");
     }
     //Give an error message and show the correct usage
     else {
@@ -102,10 +117,10 @@ public class VendingMachine{
 
                 }
             }
-            catch(ClassNotFoundException classNot){
+            catch (ClassNotFoundException classNot){
                 System.err.println("Error: Data received was not in a String class format");
             }
-        }while(!userInput.equals("Q"));
+        }while (!userInput.equals("Q"));
         //Print the list of receivedItems before quitting
         System.out.println("The summary of received items:\n"+getReceivedItems());
     }
@@ -129,83 +144,119 @@ public class VendingMachine{
   }
 
   //This will be run by the server
-  private void runServer(String args[]){
-    //First print the list of items in the stock currently
-    System.out.println("The current list of items:\n" + getItemList());
-    //Catch if client terminated the connection
+  private void runServer(String args[]) throws Exception{
+    //Create an array of threads for each thread to handle a client
     try{
-        serverSocket = new ServerSocket(Integer.parseInt(args[0]));
-        System.out.print("Waiting for client... ");
-        //Setup in and out streams
-        connection = serverSocket.accept();
-        out = new ObjectOutputStream(connection.getOutputStream());
-        out.flush();
-        in = new ObjectInputStream(connection.getInputStream());
-        System.out.println("A client is connected.");
-        //Keep on serving this client until an exception due terminating the connection is thrown
-        do{
+      serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+      //A slight modification here: Print the items after the server is established on the given port
+      //To disallow printing the item list and terminating because there is already another server on the same port
+      System.out.println("The current list of items:\n" + getItemList());
+      System.out.print("Waiting for client... ");
+      //Setup in and out streams
+      while(true){
+          //Wait while current number of threads is greater than or equal to the number of allowed threads
+          while(numOfThreads >= MAX_NUM_OF_THREADS){
             try{
-                //Get the request from the client
-                message = (String)in.readObject();
-                System.out.println("The received message:\n" + message);
-                //Use a Scanner object to get rid of line delimiters
-                Scanner readLines = new Scanner(message);
-                String request = readLines.nextLine();
-                if (request.equals("GET ITEM LIST")){
-                  // Print sent message
-                  System.out.print("Send the message:\n" + getItemList() + "\r\n");
-                  sendMessage(getItemList());
-                } else if (request.equals("GET ITEM")){
-                  //Check that the ID and quantity requested are integers. If not then catch the exception and send the client an error message
-                  try{
-                    int itemID = readLines.nextInt();
-                    int itemRequestCount = readLines.nextInt();
-                    //Don't accept negative or zero quantities
-                    if (itemRequestCount <= 0){
-                      System.out.print("Send the message:\n" + "Error: Cannot request nonpositive quantity\r\n");
-                      sendMessage("Error: Cannot request nonpositive quantity\r\n");
-                    }
-                    //Check if the item is in the stock and that there is enough availability
-                    else if (itemStock.get(itemID) != null && itemStock.get(itemID) >= itemRequestCount){
-                      itemStock.put(itemID, itemStock.get(itemID) - itemRequestCount);
-                      System.out.print("Send the message:\n" + "SUCCESS\r\n\r\n");
-                      sendMessage("SUCCESS\r\n\r\n");
-                    } else {
-                      System.out.print("Send the message:\n" + "OUT OF STOCK\r\n\r\n");
-                      sendMessage("OUT OF STOCK\r\n\r\n");
-                    }
-                  } catch (InputMismatchException e){
-                      System.out.print("Error: Cannot extract item integer values");
-                      sendMessage("Bad request content: Cannont parse integer values of items\r\n");
-                  }
-
-                } else {
-                  //ERROR
-                  sendMessage("Bad request header\r\n");
-                }
+              Thread.sleep(10);
+            }
+            catch(Exception e){
 
             }
-            catch(ClassNotFoundException classnot){
-                System.err.println("Data received in unknown format");
-            }
-        }while(true);
+          }
+          //Accept a connection from a client and print on the console
+          connection = serverSocket.accept();
+          System.out.println("A client is connected.");
+          //Create a thread to run the concurrent part of the program for the given connection from a client
+          new Thread(new VendingMachine(connection)).start();
+          //And continue waiting for other connections in the loop, while there may be active threads or not
+      }
     }
     catch(IOException ioException){
         System.out.println("The client has terminated the connection.");
         //ioException.printStackTrace();
     }
-    // And close the connections of course
     finally{
-        try{
-            in.close();
-            out.close();
-            serverSocket.close();
-        }
-        catch(IOException ioException){
-            ioException.printStackTrace();
-        }
+      try{
+          // And close the connections of course
+          in.close();
+          out.close();
+          serverSocket.close();
+      }
+      catch(Exception e){
+        System.out.println("The client has terminated the connection.");
+      }
     }
   }
+
+  //@TODO
+  //This whole method is synchronized for the time being, we could try making it sychronized for only the shared variables
+  //And see how it goes from there
+  public synchronized void run(){
+      long threadId = Thread.currentThread().getId();
+      System.out.println("Hello from thread with id " + threadId);
+      try {
+      out = new ObjectOutputStream(connection.getOutputStream());
+      out.flush();
+      in = new ObjectInputStream(connection.getInputStream());
+
+      //Keep on serving this client until an exception due terminating the connection is thrown
+      do{
+          try{
+              //Get the request from the client
+              message = (String)in.readObject();
+              System.out.println("The received message:\n" + message);
+              //Use a Scanner object to get rid of line delimiters
+              Scanner readLines = new Scanner(message);
+              String request = readLines.nextLine();
+              if (request.equals("GET ITEM LIST")){
+                // Print sent message
+                System.out.print("Send the message:\n" + getItemList() + "\r\n");
+                sendMessage(getItemList());
+              } else if (request.equals("GET ITEM")){
+                //Check that the ID and quantity requested are integers. If not then catch the exception and send the client an error message
+                try{
+                  int itemID = readLines.nextInt();
+                  int itemRequestCount = readLines.nextInt();
+                  //Don't accept negative or zero quantities
+                  if (itemRequestCount <= 0){
+                    System.out.print("Send the message:\n" + "Error: Cannot request nonpositive quantity\r\n");
+                    sendMessage("Error: Cannot request nonpositive quantity\r\n");
+                  }
+                  //Check if the item is in the stock and that there is enough availability
+                  else if (itemStock.get(itemID) != null && itemStock.get(itemID) >= itemRequestCount){
+                    itemStock.put(itemID, itemStock.get(itemID) - itemRequestCount);
+                    System.out.print("Send the message:\n" + "SUCCESS\r\n\r\n");
+                    sendMessage("SUCCESS\r\n\r\n");
+                  } else {
+                    System.out.print("Send the message:\n" + "OUT OF STOCK\r\n\r\n");
+                    sendMessage("OUT OF STOCK\r\n\r\n");
+                  }
+                } catch (InputMismatchException e){
+                    System.out.print("Error: Cannot extract item integer values");
+                    sendMessage("Bad request content: Cannont parse integer values of items\r\n");
+                }
+
+              } else {
+                //ERROR
+                sendMessage("Bad request header\r\n");
+              }
+
+          }
+          catch(ClassNotFoundException classnot){
+              System.err.println("Data received in unknown format");
+          }
+      }while(true);
+    }
+    catch(Exception e){
+      // e.printStackTrace();
+      if(e instanceof EOFException){
+
+      }
+    }
+    numOfThreads--;
+    System.out.println("Thread ends");
+  }
+
   //A generic message sending method for both client and server
   void sendMessage(String message) throws IOException{
     out.writeObject(message);
